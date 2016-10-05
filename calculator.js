fce09f98af3564added844803f6fbfaa914bcbf6
Nathan Leung <18nleung@gmail.com>
2016-10-05 01:00:58 +0000

    make files export IIFE result

--- calculator.js
+++ calculator.js
@@ -1,83 +1,91 @@
-function round(num, places) {
-	const factor = Math.pow(10, places);
-	const result = (Math.round(num * factor) / factor);
-	return result;
-}
-
-function atanDeg(num) {
-  const angle = Math.atan(num) * 180 / Math.PI;
-  return round(angle, 3);
-}
-
-function getVals() {
-	const vals = {
-  	a: document.getElementById('a').value,
-    b: document.getElementById('b').value,
-    c: document.getElementById('c').value,
-  };
-  return vals;
-}
-
-function setWidth(vals) {
-	const keys = Object.keys(vals);
-	for (let i = 0; i < keys.length; i++) {
-  	const el = document.getElementById(keys[i]);
-    const chars = el.value.length;
-    if (chars > 1) {
-    	el.style.width = (chars * 2.5 + 2.5).toString() + 'rem';
-    } else if (chars === 1) {
-      el.style.width = '5rem';
-    }
-  }
-}
-
-let inactive = 'c';
-function calculate(e) {
-  const target = e.target.id;
-  if (target === 'c') {
-    inactive = 'b';
-  } else {
-    inactive = 'c';
+const PYTHAG_CALC = (() => {
+  function round(num, places) {
+  	const factor = Math.pow(10, places);
+  	const result = (Math.round(num * factor) / factor);
+  	return result;
   }
   
-  let vals = getVals();
+  function atanDeg(num) {
+    const angle = Math.atan(num) * 180 / Math.PI;
+    return round(angle, 3);
+  }
   
-  const condition =
-  	(vals.a !== '' && vals.b !== '') ||
-    (vals.a !== '' && vals.c !== '') ||
-    (vals.b !== '' && vals.c !== '');
-    
-  if (condition) {
-  	const nums = {
-    	a: Number(vals.a),
-      b: Number(vals.b),
-      c: Number(vals.c),
+  function getVals() {
+  	const vals = {
+    	a: document.getElementById('a').value,
+      b: document.getElementById('b').value,
+      c: document.getElementById('c').value,
     };
-    // update angles in infobox
-    const angleA = MathJax.Hub.getAllJax("angle-a")[0];
-    MathJax.Hub.Queue(["Text",angleA,atanDeg(nums.a / nums.b) + '^{\\circ}']);
-    const angleB = MathJax.Hub.getAllJax("angle-b")[0];
-    MathJax.Hub.Queue(["Text",angleB,atanDeg(nums.b / nums.a) + '^{\\circ}']);
-  	switch (inactive) {
-      case 'b': {
-      	const result = Math.sqrt(Math.pow(nums.c, 2) - Math.pow(nums.a, 2));
-      	document.getElementById('b').value = round(result.toString(), 4);
-        break;
-      }
-      case 'c': {
-        const result = Math.sqrt(Math.pow(nums.a, 2) + Math.pow(nums.b, 2));
-        document.getElementById('c').value = round(result.toString(), 4);
-        break;
-      }
-    }
-  } else {
-    const angleA = MathJax.Hub.getAllJax("angle-a")[0];
-    MathJax.Hub.Queue(["Text",angleA,'0^{\\circ}']);
-    const angleB = MathJax.Hub.getAllJax("angle-b")[0];
-    MathJax.Hub.Queue(["Text",angleB,'0^{\\circ}']);
+    return vals;
   }
   
-  // get updated vals and fix widths
-  vals = getVals();
-  setWidth(vals);
-}
+  function setWidth(vals) {
+  	const keys = Object.keys(vals);
+  	for (let i = 0; i < keys.length; i++) {
+    	const el = document.getElementById(keys[i]);
+      const chars = el.value.length;
+      if (chars > 1) {
+      	el.style.width = (chars * 2.5 + 2.5).toString() + 'rem';
+      } else if (chars === 1) {
+        el.style.width = '5rem';
+      }
+    }
+  }
+  
+  let inactive = 'c';
+  function calculate(e) {
+    const target = e.target.id;
+    if (target === 'c') {
+      inactive = 'b';
+    } else {
+      inactive = 'c';
+    }
+    
+    let vals = getVals();
+    
+    const condition =
+    	(vals.a !== '' && vals.b !== '') ||
+      (vals.a !== '' && vals.c !== '') ||
+      (vals.b !== '' && vals.c !== '');
+      
+    if (condition) {
+    	const nums = {
+      	a: Number(vals.a),
+        b: Number(vals.b),
+        c: Number(vals.c),
+      };
+      // update angles in infobox
+      const angleA = MathJax.Hub.getAllJax("angle-a")[0];
+      MathJax.Hub.Queue(["Text",angleA,atanDeg(nums.a / nums.b) + '^{\\circ}']);
+      const angleB = MathJax.Hub.getAllJax("angle-b")[0];
+      MathJax.Hub.Queue(["Text",angleB,atanDeg(nums.b / nums.a) + '^{\\circ}']);
+    	switch (inactive) {
+        case 'b': {
+        	const result = Math.sqrt(Math.pow(nums.c, 2) - Math.pow(nums.a, 2));
+        	document.getElementById('b').value = round(result.toString(), 4);
+          break;
+        }
+        case 'c': {
+          const result = Math.sqrt(Math.pow(nums.a, 2) + Math.pow(nums.b, 2));
+          document.getElementById('c').value = round(result.toString(), 4);
+          break;
+        }
+      }
+    } else {
+      const angleA = MathJax.Hub.getAllJax("angle-a")[0];
+      MathJax.Hub.Queue(["Text",angleA,'0^{\\circ}']);
+      const angleB = MathJax.Hub.getAllJax("angle-b")[0];
+      MathJax.Hub.Queue(["Text",angleB,'0^{\\circ}']);
+    }
+    
+    // get updated vals and fix widths
+    vals = getVals();
+    setWidth(vals);
+  }
+  
+  return {
+    calculate,
+    getVals,
+    atanDeg,
+  };
+})();
--- canvas.js
+++ canvas.js
@@ -1,64 +1,71 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-// make sure width stays constant even when inputs grow
-canvas.style.width = canvas.offsetWidth.toString() + 'px';
-// coordinate system width
-const width = canvas.width;
-
-function updateCanvas() {
-  // clear canvas before redrawing
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  const vals = getVals();
-  const valsArr = [vals.a,vals.b,vals.c];
+const PYTHAG_CANVAS = (() => {
+  const { getVals, atanDeg } = PYTHAG_CALC;
   
-  const condition = (vals.a !== '' && vals.b !== '' && vals.c !== '');
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  // make sure width stays constant even when inputs grow
+  canvas.style.width = canvas.offsetWidth.toString() + 'px';
+  // coordinate system width
+  const width = canvas.width;
   
-  if (condition) {
-    // so scaled hypotenuse = width of canvas minus some padding
-    const padding = 50;
-    const scale = (width - padding) / vals.c;
-    const sideLengths = valsArr.map((el) => {
-      return el * scale;
-    });
+  function updateCanvas() {
+    // clear canvas before redrawing
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const vals = getVals();
+    const valsArr = [vals.a,vals.b,vals.c];
     
-    const offsetBottom = (width - sideLengths[0]) / 2;
-    const offsetLeft = (width - sideLengths[1]) / 2;
+    const condition = (vals.a !== '' && vals.b !== '' && vals.c !== '');
     
-    // draw triangle
-    ctx.beginPath();
-    // right angle
-    ctx.moveTo(offsetLeft, width - offsetBottom);
-    // top angle
-    ctx.lineTo(offsetLeft, width - sideLengths[0] - offsetBottom);
-    // angle on right side (not right angle :P)
-    ctx.lineTo(sideLengths[1] + offsetLeft, width - offsetBottom);
-    // back to right angle
-    ctx.lineTo(offsetLeft, width - offsetBottom);
-    ctx.stroke();
-    
-    // draw labels
-    let fontOffset = width / 18;
-    const lineHeight = width / 25;
-    ctx.font = `${lineHeight * 0.8}px Raleway`;
-    // Angles
-    ctx.fillText('a', offsetLeft - fontOffset, width - sideLengths[0] - offsetBottom);
-    ctx.fillText('b', sideLengths[1] + offsetLeft, width - offsetBottom + fontOffset);
-    ctx.fillText('c', offsetLeft - fontOffset, width - offsetBottom + fontOffset);
-    // Sides
-    fontOffset = width / 16; // a little bigger offset for the angles
-    ctx.fillText('A', (sideLengths[1]) / 2 + offsetLeft, width - offsetBottom + fontOffset);
-    ctx.fillText('B', offsetLeft - fontOffset, width - (sideLengths[0] / 2) - offsetBottom);
-    ctx.fillText('C', (sideLengths[1]) / 2 + offsetLeft + fontOffset, width - (sideLengths[0] / 2) - offsetBottom);
-    // Information
-    const textBoxSize = width / 3;
-    ctx.fillText(`A: ${vals.a} units`, width - textBoxSize, 2 * lineHeight);
-    ctx.fillText(`B: ${vals.b} units`, width - textBoxSize, 3 * lineHeight);
-    ctx.fillText(`C: ${vals.c} units`, width - textBoxSize, 4 * lineHeight);
-    // It's easier to just print this below the inputs
-    /*
-    ctx.fillText(`a = atan(A/B) = ${atanDeg(vals.a/vals.b)}°`, width - textBoxSize, 5 * lineHeight);
-    ctx.fillText(`b = atan(B/A) = ${atanDeg(vals.b/vals.a)}°`, width - textBoxSize, 6 * lineHeight);
-    ctx.fillText(`c = 90°`, width - textBoxSize, 7 * lineHeight);
-    */
+    if (condition) {
+      // so scaled hypotenuse = width of canvas minus some padding
+      const padding = 50;
+      const scale = (width - padding) / vals.c;
+      const sideLengths = valsArr.map((el) => {
+        return el * scale;
+      });
+      
+      const offsetBottom = (width - sideLengths[0]) / 2;
+      const offsetLeft = (width - sideLengths[1]) / 2;
+      
+      // draw triangle
+      ctx.beginPath();
+      // right angle
+      ctx.moveTo(offsetLeft, width - offsetBottom);
+      // top angle
+      ctx.lineTo(offsetLeft, width - sideLengths[0] - offsetBottom);
+      // angle on right side (not right angle :P)
+      ctx.lineTo(sideLengths[1] + offsetLeft, width - offsetBottom);
+      // back to right angle
+      ctx.lineTo(offsetLeft, width - offsetBottom);
+      ctx.stroke();
+      
+      // draw labels
+      let fontOffset = width / 18;
+      const lineHeight = width / 25;
+      ctx.font = `${lineHeight * 0.8}px Open Sans`;
+      // Angles
+      ctx.fillText('a', offsetLeft - fontOffset, width - sideLengths[0] - offsetBottom);
+      ctx.fillText('b', sideLengths[1] + offsetLeft, width - offsetBottom + fontOffset);
+      ctx.fillText('c', offsetLeft - fontOffset, width - offsetBottom + fontOffset);
+      // Sides
+      fontOffset = width / 16; // a little bigger offset for the angles
+      ctx.fillText('A', (sideLengths[1]) / 2 + offsetLeft, width - offsetBottom + fontOffset);
+      ctx.fillText('B', offsetLeft - fontOffset, width - (sideLengths[0] / 2) - offsetBottom);
+      ctx.fillText('C', (sideLengths[1]) / 2 + offsetLeft + fontOffset, width - (sideLengths[0] / 2) - offsetBottom);
+      // Information
+      const textBoxSize = width / 3;
+      ctx.fillText(`A: ${vals.a} units`, width - textBoxSize, 2 * lineHeight);
+      ctx.fillText(`B: ${vals.b} units`, width - textBoxSize, 3 * lineHeight);
+      ctx.fillText(`C: ${vals.c} units`, width - textBoxSize, 4 * lineHeight);
+      // It's easier to just print this below the inputs
+      /*
+      ctx.fillText(`a = atan(A/B) = ${atanDeg(vals.a/vals.b)}°`, width - textBoxSize, 5 * lineHeight);
+      ctx.fillText(`b = atan(B/A) = ${atanDeg(vals.b/vals.a)}°`, width - textBoxSize, 6 * lineHeight);
+      ctx.fillText(`c = 90°`, width - textBoxSize, 7 * lineHeight);
+      */
+    }
   }
-}
+  return {
+    updateCanvas,
+  };
+})();
